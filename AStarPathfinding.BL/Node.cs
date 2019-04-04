d22b069aca2553b239153245c537652dd9a7ab78
RasmusAgergaard <rasmusap@gmail.com>
2019-04-04 13:45:13 +0200

    Classes created

---
+++ AStarPathfinding.BL/Node.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStarPathfinding.BL
+{
+    class Node
+    {
+        public enum nodeState { untestet, open, closed}
+
+        public Point Location { get; private set; }
+        public bool IsWalkable { get; set; }
+        public float G { get; private set; }
+        public float H { get; private set; }
+        public float F { get { return this.G + this.H; } }
+        public nodeState State { get; set; }
+        public Node ParentNode { get; set; }
+
+    }
+}
---
+++ AStarPathfinding.BL/SearchParameters.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AStarPathfinding.BL
+{
+    public class SearchParameters
+    {
+        //bool[,] grid = new bool[7, 5];
+
+        public Point StartLocation { get; set; }
+        public Point EndLocation { get; set; }
+        public bool[,] Map { get; set; }
+
+        private bool Search(Node currentNode)
+        {
+            return false;
+        }
+    }
+}
